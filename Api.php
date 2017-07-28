d94a97b49e9d3198273381f162a572fe3474838e
bassrelic <h_p_r@web.de>
2017-07-28 23:09:07 +0200

    Added description on how to use + functionality
    The site now gives an explanation on how to use the site.
    
    After submitting the API-Key the site shows account name and the type of acces the account has.

--- Api.php
+++ Api.php
@@ -24,6 +24,11 @@ Using the Guild Wars 2 API to get a grasp on how to get data from an external si
         ?>
 
         <div class="section">
+            <h1><span>Test of the GW2 API</span></h1>
+            <h2>To get your account data, you must insert your API-Key. <br /> 
+                You can get it from <a href="https://account.arena.net/login"> The official GW2 Website</a> <br />
+                Just login, go to applications, create a new Key, copy-paste it into the form below and klick submit.
+            </h2>
             <form action="api.php" method="post">
                 <p>API-Key:<input type="text" name="Key"/></p>
                 <p><input type="submit" name="submit" value="submit"</p>
@@ -37,19 +42,30 @@ Using the Guild Wars 2 API to get a grasp on how to get data from an external si
             $api="https://api.guildwars2.com/v2/account";
             $token="?access_token=";
             $url=$api.$token.$key;
-//            echo "".$url;
             $json = file_get_contents($url);
             $data = json_decode($json);
-            echo $data->{name};                 // get name
-            echo $data->{output_item_id};
-            echo $data->{output_item_count};
-            echo $data->{min_rating};
-            echo $data->{time_to_craft_ms};
-            echo $data->{disciplines}[1];       // get element of array
             
-//            
-//            echo $json;
-            var_dump($json);
+            $i=0;
+            foreach ($data as $value){
+                switch ($i) {
+                    case 1:
+                        echo "Account name: " , $value;
+                        echo nl2br("\n");
+                    break;
+                    case 7:
+                        echo "Acces to: " , $value;
+                        echo nl2br("\n");
+                    break;
+
+                    default:
+                    break;
+                }
+                $i++;
+            }
+            
+//            echo $data->{name};                 // get name
+      
+//            var_dump($json);                    // dumps the whole object
             ?>
                 
         </div>
