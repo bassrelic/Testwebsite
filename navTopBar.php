668618e471c98e6aa818aad406417f704afdfc73
Sebastian <h_p_r@web.de>
2020-04-02 23:30:49 +0200

    Correct Spelling mistake

--- navTopBar.php
+++ navTopBar.php
@@ -6,10 +6,10 @@ Localized Header
         <ul id="navTopBar">
             <li></li>
             <li><a href="index.php">Home</a></li>
-            <li><a href="projects.php">Projekte</a></li>
+            <li><a href="projects.php">Projekts</a></li>
             <li><a href="#">Coming Soon</a></li>
             <li><a href="#">Coming Soon</a></li>
             <li></li>
         </ul>
     </nav>
-</div>
+</div>  
